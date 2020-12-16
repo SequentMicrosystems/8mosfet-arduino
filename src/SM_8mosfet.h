6df7a4cecb50c7186a7bbdbbdaec2776e620ceb3
Alexandru Burcea <olcitu@gmail.com>
2020-12-17 01:00:37 +0200

    Change methods to read and write

--- src/SM_8mosfet.h
+++ src/SM_8mosfet.h
@@ -9,14 +9,14 @@ class SM_8mosfet
     SM_8mosfet();
     uint8_t begin(uint8_t);
     uint8_t begin();
-    uint8_t set(uint8_t, uint8_t);
-    uint8_t set(int, int);
-    uint8_t get(uint8_t);
-    uint8_t setAll(uint8_t);
-    uint8_t getAll();
+    uint8_t writeChannel(uint8_t, uint8_t);
+    uint8_t writeChannel(int, int);
+    uint8_t readChannel(uint8_t);
+    uint8_t writeAll(uint8_t);
+    uint8_t readAll();
      
-    inline uint8_t get(int channel) { return get((uint8_t)channel); }
-    inline uint8_t setAll(int val) { return setAll((uint8_t)val); }
+    inline uint8_t readChannel(int channel) { return readChannel((uint8_t)channel); }
+    inline uint8_t writeAll(int val) { return writeAll((uint8_t)val); }
   private:
     uint8_t _hwAdd;
     uint8_t rawRead(uint8_t *val);
