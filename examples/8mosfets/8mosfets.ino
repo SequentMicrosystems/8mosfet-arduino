c3e8e1d39fe0997376e62963f5ce4cb8cc84a14d
Alexandru Burcea <olcitu@gmail.com>
2020-12-17 01:08:02 +0200

    Change set/get methods with read/write

--- examples/8mosfets/8mosfets.ino
+++ examples/8mosfets/8mosfets.ino
@@ -4,6 +4,9 @@
  * A 8-Mosfets card controling example
  * This example control 2 cards.
  * 
+ * Hardware requarements except your favorite processor: 
+ * 1) 8-MOSFETS 8-Layer Stackable Card for Raspberry Pi: https://sequentmicrosystems.com/product/raspberry-pi-mosfets-stackable-hat/
+ * 2) S-Bridge : https://sequentmicrosystems.com/product/raspberry-pi-replacement-card/
  */
 #include <SM_8mosfet.h>
 
@@ -20,16 +23,17 @@ void setup() {
   }
   else
   {
-   Serial.println(" Fail!");
+    Serial.println(" Fail!");
   }
-   Serial.print("8-Mosfets card 2 init");
-   if(0 == mos2.begin(1)) // set the stack level to 1
-   {
+  
+  Serial.print("8-Mosfets card 2 init");
+  if(0 == mos2.begin(1)) // set the stack level to 1
+  {
     Serial.println(" Done");
   }
   else
   {
-   Serial.println(" Fail!");
+    Serial.println(" Fail!");
   }
 
 }
@@ -39,18 +43,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   for(i = 1; i < 9; i++) //turn mosfets ON, one by one 
   {
-    mos1.set(i, 1);
-    mos2.set(i, 1);
+    mos1.writeChannel(i, 1);
+    mos2.writeChannel(i, 1);
     delay(250);
   }
-  //Serial.println(mos.getAll());
+  //Serial.println(mos.readAll());
   delay (1000);
   for(i = 1; i < 9; i++)//turn mosfets OFF, one by one 
   {
-    mos1.set(i, 0);
-    mos2.set(i, 0);
+    mos1.writeChannel(i, 0);
+    mos2.writeChannel(i, 0);
     delay(250);
   }
-  //Serial.println(mos.getAll());
+  //Serial.println(mos.readAll());
   delay (1000);
 }
